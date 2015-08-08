6cf40e9a711ebfa7a092f2ff106f7c67584c326a
Archimedes Trajano <arch@trajano.net>
2015-08-08 00:13:06 -0400

    x

--- test-fragment/src/main/java/net/trajano/test/MyApplication.java
+++ test-fragment/src/main/java/net/trajano/test/MyApplication.java
@@ -3,14 +3,25 @@
  */
 package net.trajano.test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  * @author Archimedes Trajano
- *
  */
 @ApplicationPath("/V1")
-public class MyApplication extends Application{
+public class MyApplication extends Application {
 
+    /* (non-Javadoc)
+     * @see javax.ws.rs.core.Application#getClasses()
+     */
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        return new HashSet<Class<?>>(Arrays.asList(Resource.class));
+    }
 }
