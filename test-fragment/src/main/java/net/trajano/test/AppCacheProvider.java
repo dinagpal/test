88db5201b6b3d1c7dde934f93fed9b51a77095ac
Archimedes Trajano <arch@trajano.net>
2015-08-15 00:29:38 -0400

    Try and dump the data
    Example for https://github.com/payara/Payara/issues/397

--- test-fragment/src/main/java/net/trajano/test/AppCacheProvider.java
+++ test-fragment/src/main/java/net/trajano/test/AppCacheProvider.java
@@ -1,6 +1,11 @@
 package net.trajano.test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.cache.CacheManager;
 import javax.cache.Caching;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -8,8 +13,24 @@ import javax.ejb.Startup;
 @Singleton
 @Startup
 public class AppCacheProvider {
-	@PostConstruct
-	public void init() {
-		Caching.getCachingProvider().getCacheManager();
-	}
+
+    @PostConstruct
+    public void init() {
+
+        final CacheManager cacheManager = Caching.getCachingProvider()
+            .getCacheManager();
+        final Set<String> cacheNames = new HashSet<>();
+        for (final String name : cacheManager.getCacheNames()) {
+            cacheNames.add(name);
+        }
+        System.out.println(cacheNames);
+    }
+
+    @PreDestroy
+    public void contextDestroyed() {
+
+        Caching.getCachingProvider()
+            .close(Thread.currentThread()
+                .getContextClassLoader());
+    }
 }
